7bb086a06db65d1fb893fd6607dde8fcfd2931cb
anwrat <anweshrwt22@gmail.com>
2025-05-09 17:52:42 +0545

    Week 3 materials before section

---
+++ Codes/Week 3/dogage.py
@@ -0,0 +1,12 @@
+# Each year for a human is like 7.18 years for a dog
+DOG_YEARS_MULTIPLIER = 7.18  
+
+def main():
+    age=float(input("Enter an age in calendar years: "))
+    print(f"That's {age*DOG_YEARS_MULTIPLIER} in dog years!")
+
+
+
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()
---
+++ Codes/Week 3/sumoftwonum.py
@@ -0,0 +1,10 @@
+def main():
+    print("This program adds two numbers")
+    num1=input("Enter the first number: ")
+    num1=int(num1)
+    num2=input("Enter the second number: ")
+    num2=int(num2)
+    total=num1+num2
+    print(f"The total is {total}")
+if __name__=="__main__":
+    main()
